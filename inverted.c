390cfd7a62b303a08c610bead171ead9da5d16e0
vanmumi <vanvanmo@hotmail.com>
2017-06-01 23:24:09 +1000

    screw seg faults.

--- inverted.c
+++ inverted.c
@@ -7,37 +7,46 @@
     Generates an "inverted index" that provides a list (set) of urls for every word in a given colleciton of pages
     Need to "normalise" words by removing leading & trailing spaces
         and converting all characters to lowercase before inserting words into index
-    In each list (set) duplicatesa are not allowed.
+    In each list (set) duplicates are not allowed.
     List is ordered in asceding alphabetical order
 */
 
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
- #include "BSTree.h"
- #include "graph.h"
- #include "queue.h"
- #include "readData.h"
+#include "BSTree.h"
+#include "graph.h"
+#include "queue.h"
+#include "readData.h"
 
- int main(int argc, char * argv[])
- {
-     Queue q = newQueue();
-     getCollection(q);
+int main(int argc, char * argv[])
+{
+    Queue q = newQueue();
+    getCollection(q);
 
-     BSTNode tree = getBSTree(/*something here*/); //from readData
+    BSTNode tree = NULL; 
+    tree = getInvertedList(tree, q); 
+    if(tree == NULL) {
+       return EXIT_FAILURE; // LMAO FIX THIS MAYBE idk if this is how to do it or not 
+    }
 
+    disposeQueue(q);
+    disposeTree(tree);
 
+    return EXIT_SUCCESS;
+}
 
-     disposeQueue(q);
-     disposeTree(tree);
+void printInv(BSTNode treeNode) {
 
-     return EXIT_SUCCESS;
- }
+    FILE *fp;
 
- void printInv(BSTNode treeNode)
- {
-     BSTtoFile();
-
-     
- }
+    // checks if file is accessable 
+    if((fp = fopen("invertedIndex.txt", "w")) != NULL) {
+        // print to file
+        BSTreeInfixPrint(treeNode, fp);
+        // close file
+        fclose(fp);
+    }
+    return;
+}
--- readData.c
+++ readData.c
@@ -8,7 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "readData.h"
+#include "linkedlist.h"
 #include "set.h"
 
 #define size 50 
@@ -35,8 +37,7 @@ void getCollection(Queue q) {
     fclose(fp);
 }
 
-void getGraph(Graph g, Queue q)
-{
+void getGraph(Graph g, Queue q) {
 	char * fileName;
 	char buffer[size];
 	//char temp[size];
@@ -56,7 +57,6 @@ void getGraph(Graph g, Queue q)
 
 		//Look for "Section 1" and "end"
 		//Not at the end of the file yet
-
 		while((fscanf(fp, "%s", buffer) != EOF) && strcmp(buffer, "#end") != 0) {
 			//Look for "start" and "section1"
 			if((strcmp(buffer, "#start") != 0) && (strcmp(buffer, "Section-1") != 0)) {
@@ -69,6 +69,98 @@ void getGraph(Graph g, Queue q)
 		fclose(fp);
 		free(fileName);
 	}
+}
+
+char * normalise(char * badString)
+{
+
+	//If there is a trailing newLine -> replace it with null character for end of string
+	//Check for extra full stops
+	//Copy everything into the temporary string
+	//Lowercase all the characters in the temporary string
+	//return the nice new string
+
+	//Convert everything into lowercase
+	int i = 0;
+	int len = strlen(badString);
+	for(i = 0; i < len; i++) badString[i] = tolower(badString[i]);
+	//Find the newline characters and spaces
+
+	//Strchr - returns pointer to first occurence of char otherwise NULL
+	char * tmp;
+	tmp = strchr(badString, '\n');
+	if(tmp != NULL) * tmp = '\0';
+
+	//Make a temporary string to search for full stops
+	char * tmp2 = malloc(sizeof(char) * strlen(badString));
+
+	char * pointer = strchr(badString, '.');
+	while(pointer != NULL) {
+		strcpy(tmp2, pointer + 1);
+		strcpy(pointer, tmp2);
+	}
+	free(tmp2);
+
+	//badString is now 'normalised';
+	return badString;
+}
+
+
+BSTNode getInvertedList(BSTNode treeNode, Queue q) {
+	char * fileName;
+	char buffer[size];
+	char *temp[size];
+	int i = 0;
+
+	while(!emptyQueue(q)) {
+		//Increment -> move to next file
+		//leaveQueue -> returns string at the front of the queue
+		fileName = leaveQueue(q);
+
+		//Add the .txt suffix to the current file
+		strcat(fileName, ".txt");
+
+		FILE * fp;
+		if((fp = fopen(fileName, "r")) == NULL)  return NULL; 
+
+		printf("site 1: %s\n", fileName);
+		//Look for "Section 1" and "end"
+		//Not at the end of the file yet
+		while((fscanf(fp, "%s", buffer) != EOF) && strcmp(buffer, "#end") != 0) {
+			//Look for "start" and "section1"
+			if((strcmp(buffer, "#start") != 0) && (strcmp(buffer, "Section-1") != 0)) {
+				strcat(buffer, ".txt");
+				temp[i] = buffer;
+				printf("temp:%s\n", temp[i]);
+				i++;
+			}
+		}
+
+		int i=0;
+		int j=0;
+		int k=0;
+
+
+		//Look for "Section 2" and "end"
+		//Not at the end of the file yet
+		while((fscanf(fp, "%s", buffer) != EOF) && strcmp(buffer, "#end") != 0) {
+
+			//Look for "start" and "section2" and "section1" 
+			if((strcmp(buffer, "#start") != 0) && (strcmp(buffer, "Section-2") != 0) && (strcmp(buffer, "Section-1"))) {
+				// create node
+				
+		printf("%d site 2: %s\n", i++, buffer);
+				Node new = newNode(fileName);
+			printf("%d site 3: %s\n", j++, buffer);
+				// normalise string and insert into tree. 
+				insertBSTree(treeNode, normalise(buffer), new); // !!!!!!!!!!!!!!!!!!!
+printf("%d site 4: %s\n", k++, buffer);
+			}
+		}
+		fclose(fp);
+		free(fileName);
+	}
+	return treeNode;
 }
 
 
